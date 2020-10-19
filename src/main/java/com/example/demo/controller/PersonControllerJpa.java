8ee2fdbdcb1869685658a064639d4991c16862e2
Sebastian Serritella <sebaserritella@gmail.com>
2020-10-19 15:07:06 -0300

    Add annotations for swagger

--- src/main/java/com/example/demo/controller/PersonControllerJpa.java
+++ src/main/java/com/example/demo/controller/PersonControllerJpa.java
@@ -2,6 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Person;
 import com.example.demo.repository.PersonRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("persons1")
+@RequestMapping("persons")
+@Api(value = "PersonControllerJpa", description = "REST API Person description")
 public class PersonControllerJpa {
 
     @Qualifier("personRepository")
@@ -25,6 +30,12 @@ public class PersonControllerJpa {
         return repository.findAll();
     }
 
+    @ApiOperation(value = "Get list of persons by surname ", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @GetMapping("/BySurname/{surname}")
     public Iterable<Person> getPersonsBySurname(@PathVariable String surname) {
         return repository.findAllBySurname(surname);
@@ -40,9 +51,13 @@ public class PersonControllerJpa {
         return repository.save(p);
     }
 
+    @ApiOperation(value = "Get specific Person in the System ", response = Person.class, tags = "getPerson")
+    @RequestMapping(value = "/getPerson/{name}")
+    public Person getPerson(@PathVariable(value = "name") String name) {
+        return repository.findAllByName(name).get(0);
+    }
     @GetMapping("/{id}")
     Optional<Person> one(@PathVariable Long id) {
-
         return repository.findById(id);
     }
 
